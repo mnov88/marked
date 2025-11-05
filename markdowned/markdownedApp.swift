1081de80d2aab6c3ec61dd2addc863a42d8a203f
mnov88 <47901419+mnov88@users.noreply.github.com>
2025-11-05 12:52:18 +0100

    Initial Commit

---
+++ markdowned/markdownedApp.swift
@@ -0,0 +1,17 @@
+//
+//  markdownedApp.swift
+//  markdowned
+//
+//  Created by Milos Novovic on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct markdownedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ markdownedTests/markdownedTests.swift
@@ -0,0 +1,16 @@
+//
+//  markdownedTests.swift
+//  markdownedTests
+//
+//  Created by Milos Novovic on 05/11/2025.
+//
+
+import Testing
+
+struct markdownedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
